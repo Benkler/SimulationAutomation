5e5f01dab05ffeaf2a9980a144f4b7ebdcad29b8
Niko Benkler <benklerniko@t-online.de>
2020-07-17 17:41:27 +0200

    handle method argument not valid exception in rest interface

--- src/main/java/org/simulationautomation/rest/ExceptionHandlerAdvice.java
+++ src/main/java/org/simulationautomation/rest/ExceptionHandlerAdvice.java
@@ -1,10 +1,17 @@
 package org.simulationautomation.rest;
 
+import java.time.LocalDate;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.simulationautomation.kubernetesclient.exceptions.RestClientException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -28,6 +35,23 @@ public class ExceptionHandlerAdvice extends ResponseEntityExceptionHandler {
         "Kubernetes Rest Client Exception Exception caught with error message:" + e.getMessage());
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
 
+  }
+
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+      HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+    Map<String, Object> body = new LinkedHashMap<>();
+    body.put("timestamp", LocalDate.now());
+    body.put("status", status.value());
+
+    List<String> errors = ex.getBindingResult().getFieldErrors().stream()
+        .map(x -> x.getDefaultMessage()).collect(Collectors.toList());
+
+    body.put("errors", errors);
+
+    return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
   }
 
 }
